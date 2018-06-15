783ce5f276087167dbf3ad73c470d266df054c50
bodie101 <keysbw@g.toiohomai.ac.nz>
2018-06-15 15:32:57 +1200

    Made the main page and took the side menu out, Also made the log out button go back to the login screen

---
+++ scripts.js
@@ -0,0 +1,12 @@
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "auto";
+    document.getElementById("main").style.marginLeft = "auto";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "auto";
+    document.getElementById("main").style.marginLeft= "auto";
+    document.body.style.backgroundColor = "white";
+}
